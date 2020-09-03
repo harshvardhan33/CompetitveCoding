84a94d7326bc75423ac6ca624eb4606a3d20b8aa
Harsh Vardhan <47622619+harshvardhan33@users.noreply.github.com>
2020-09-03 13:29:10 +0530

    Add files via upload

---
+++ wordSearch2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 27 21:23:32 2020
+
+@author: harshvardhan
+"""
+
+
+
+
+
+
+
+def wordExist(w,temp_board):
+    for i in w:
+        for row in range(board:
+            
+
+
+
+
+board = [
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+words = ["oath","pea","eat","rain"]
+
+for i in words:
+    temp_board = [[0 for i in range(len(baords[0]))]for j in range(len(boards))]
+    pass
